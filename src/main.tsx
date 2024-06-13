9fb83cbe28c2e5dfb064eb521764ffd75870bc73
rukavyshnykov <82444499+rukavyshnykov@users.noreply.github.com>
2024-06-13 13:32:28 +0300

    feature: added font

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,8 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 
 import './styles/index.scss'
+import '@fontsource/roboto/400.css'
+import '@fontsource/roboto/700.css'
 
 import { App } from './App'
 
